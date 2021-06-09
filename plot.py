054b6ef950e2ba6e166e68100e205080ba6d545f
Robert Gericke <49953838+robertgericke@users.noreply.github.com>
2021-06-09 14:29:25 +0200

    plot access

---
+++ plot.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = (11.7,8.3)
+plt.rcParams["figure.dpi"] = 600
+data = np.loadtxt('access.txt')
+
+plt.plot(data)
+plt.savefig('plot.png')
+plt.yscale('log')
+plt.savefig('plot_log.png')
+
+plt.clf()
+
+sort = np.flip(np.sort(data))
+plt.plot(sort)
+plt.savefig('sort.png')
+plt.yscale('log')
+plt.savefig('sort_log.png')
+
+plt.clf()
+
+points = data[0:793470]
+sampled = data[1586981:2380450]
+plt.plot(points)
+plt.plot(sampled / 2)
+plt.savefig('word_distribution.png')
+plt.yscale('log')
+plt.savefig('word_distribution_log.png')
