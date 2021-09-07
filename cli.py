011999758a00ee4311e5b8e7eb37018f707a3bbc
Robert Gericke <49953838+robertgericke@users.noreply.github.com>
2021-09-08 00:37:27 +0200

    refactor cli

--- cli.py
+++ cli.py
@@ -3,14 +3,22 @@ from torch import cuda
 
 def parse_arguments():
     parser = ArgumentParser()
-    parser.add_argument('dataset', type=str, help='dataset directory')
+    parser.add_argument('dataset', type=str, help='directory of training data')
     parser.add_argument('vocab', type=str, help='vocabulary file')
-    parser.add_argument('-ts', '--testset', type=str, help='directory of testdata')
-    # training options
-    training = parser.add_argument_group('training')
-    training.add_argument('-e', '--epochs', default=2, type=int, help='number of epochs to run')
-    training.add_argument('-b', '--batch_size', default=128, type=int, help='number of sentences per batch')
-    training.add_argument('-s', '--samples', default=8192, type=int, help='number of samples for softmax loss')
+    parser.add_argument('-ts', '--testset', type=str, help='directory of test data')
+    # cuda options
+    cudaoptions = parser.add_argument_group('cuda')
+    cudaoptions.add_argument('--no_cuda', default=cuda.is_available(), dest='cuda', action='store_false', help='disable CUDA training')
+    cudaoptions.add_argument('-di', '--device_ids', nargs='+', type=int, help='IDs of cuda devices for each worker')
+    # distribution options
+    distribution = parser.add_argument_group('distribution')
+    distribution.add_argument('-r', '--role', type=str, help='scheduler or server')
+    distribution.add_argument('-ru', '--root_uri', default='127.0.0.1', type=str, help='adress of the scheduler node')
+    distribution.add_argument('-rp', '--root_port', default='9091', type=str, help='port of the scheduler node')
+    distribution.add_argument('-nn', '--nodes', default=1, type=int, help='number of local server nodes to create')
+    distribution.add_argument('-nw', '--workers_per_node', default=1, type=int, help='number of worker threads per node')
+    distribution.add_argument('-ws', '--world_size', type=int, help='total number of server nodes')
+    distribution.add_argument('-sd', '--sync_dense', default=1, type=int, help='synchronize dense model parameters every n steps')
     # elmo options
     elmo = parser.add_argument_group('elmo')
     elmo.add_argument('-ed', '--embedding_dim', default=128, type=int, help='dimension of the word embeddings')
@@ -19,23 +27,16 @@ def parse_arguments():
     elmo.add_argument('-rd', '--recurrent_dropout', default=0.1, type=float, help='recurrent dropout for each lstm')
     elmo.add_argument('-fd', '--dropout', default=0.1, type=float, help='final dropout for elmo representations')
     elmo.add_argument('-sl', '--max_sequence_length', default=400, type=int, help='maximum sequence length (clipped above)')
-    # distribution options
-    distribution = parser.add_argument_group('distribution')
-    distribution.add_argument('-r', '--role', default='scheduler', type=str, help='scheduler or server')
-    distribution.add_argument('-ru', '--root_uri', default='127.0.0.1', type=str, help='adress of the scheduler node')
-    distribution.add_argument('-rp', '--root_port', default='9091', type=str, help='port of the scheduler node')
-    distribution.add_argument('-nn', '--nodes', default=1, type=int, help='number of local server instances to create')
-    distribution.add_argument('-nw', '--workers_per_node', default=1, type=int, help='number of worker threads per node')
-    distribution.add_argument('-ws', '--world_size', default=0, type=int, help='total number of server instances')
-    distribution.add_argument('-sd', '--sync_dense', default=1, type=int, help='synchronize dense model parameters every n steps')
-    # cuda options
-    cudaoptions = parser.add_argument_group('cuda')
-    training.add_argument('--no_cuda', default=cuda.is_available(), dest='cuda', action='store_false', help='disable CUDA training')
-    cudaoptions.add_argument('-di', '--device_ids', nargs='+', type=int, help='IDs of cuda devices for each worker')
+    # training options
+    training = parser.add_argument_group('training')
+    training.add_argument('-e', '--epochs', default=2, type=int, help='number of epochs to run')
+    training.add_argument('-b', '--batch_size', default=128, type=int, help='number of sentences per batch')
+    training.add_argument('-s', '--samples', default=8192, type=int, help='number of samples for softmax loss')
 
     args = parser.parse_args()
-    if args.world_size == 0:
+    if args.world_size is None:
         args.world_size = args.nodes
+        args.role = 'scheduler'
     return args
 
 
